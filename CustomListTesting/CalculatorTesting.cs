37af4b08f6c96961850984690398aae71f86dc42
danielfernandez63 <danmanfernan@gmail.com>
2018-07-05 11:38:22 -0500

    setting initial project and testing project

---
+++ CustomListTesting/CalculatorTesting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+
+namespace CustomListTesting
+{
+    [TestClass]
+    public class CalculatorTesting
+    {
+        [TestMethod]
+        
+            public void AddPositiveNumbers()
+            {
+                int firstNumber = 5;
+                int secondNumber = 6;
+
+                Calculator calculate = new Calculator();
+
+                int sum = calculate.Add(firstNumber, secondNumber);
+
+                Assert.AreEqual(11, sum);
+
+            }
+
+        [TestMethod]
+        public void AddNegativeNumbers()
+        {
+            int firstNumber = -5;
+            int secondNumber = -6;
+
+            Calculator calculate = new Calculator();
+            int sum = calculate.Add(firstNumber, secondNumber);
+
+            Assert.AreEqual(-11, sum);
+
+
+        }
+    
+    }
+}
---
+++ CustomListTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CustomListTesting")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8610d7d-5950-42db-b66c-406015c85d76")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
